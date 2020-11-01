db87bf95124e4192f00422ba18006b4130730022
Liam Thursfield <liam@DESKTOP-KSUEFOV.localdomainliam.thursfield@nexuspoint.co.uk>
2020-11-01 08:57:46 +0000

    Fix admin layout - script

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,8 +18,6 @@
         <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
         <!-- Argon CSS -->
         <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
-
-        {{ mix('js/app.js') }}
     </head>
     <body class="{{ $class ?? '' }}">
         @auth()
@@ -41,5 +39,6 @@
 
         <!-- Argon JS -->
         <script src="{{ asset('argon') }}/js/argon.js?v=1.0.0"></script>
+        <script src="{{ mix('js/app.js') }}></script>
     </body>
 </html>
